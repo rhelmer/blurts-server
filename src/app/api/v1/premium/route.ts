0891c00efbd49caa3302b58c1725ca1f3f74209d
Robert Helmer <rhelmer@mozilla.com>
2023-05-30 13:31:57 -0700

    move premium welcome to user area, and modify route in callback API correctly

--- src/app/api/v1/premium/route.ts
+++ src/app/api/v1/premium/route.ts
@@ -3,15 +3,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
     const email = searchParams.get("email");
+    console.debug("TODO validate:", email);
 
     const url = request.nextUrl.clone();
     url.searchParams.delete("email");
-    url.pathname = "/premium-welcome";
+    url.pathname = "/user/premium-welcome";
     return NextResponse.redirect(url);
   } catch (e) {
     return NextResponse.json({ success: false }, { status: 500 });
